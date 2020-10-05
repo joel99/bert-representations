e17d93a96c86869bdc770d3e63b3e02064a6e89b
vvivek921 <vsrivivek9+blr@gmail.com>
2020-10-05 17:05:29 -0400

    ner

--- src/run_finetuning_ner.py
+++ src/run_finetuning_ner.py
@@ -3,17 +3,23 @@ from transformers import GlueDataTrainingArguments as DataTrainingArguments
 from transformers import (
     Trainer,
     glue_tasks_num_labels,
+    AutoConfig,
+    AutoModelForTokenClassification,
+    AutoTokenizer,
+    EvalPrediction,
+    HfArgumentParser,
+    TrainingArguments,
+    set_seed,
 )
 
 from src import (
     get_eval_metrics_func,
     logger
 )
+from utils_ner import Split, TokenClassificationDataset, TokenClassificationTask
 
 def run_ner(cfg, model_args, training_args, tokenizer, ckpt_path=None):
     r"""
         cfg: YACS cfg node
         ckpt_path: Unsupported
     """
-    task_name = "NER"
-    raise Exception()
