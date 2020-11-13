4fbccc2bb2908b1618d53b436225ab24db23f41e
vvivek921 <vsrivivek9+blr@gmail.com>
2020-11-13 16:04:13 -0500

    cka

---
+++ src/utils/cka.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+
+def _debiased_dot_product_similarity_helper(
+    xty, sum_squared_rows_x, sum_squared_rows_y, squared_norm_x, squared_norm_y,
+    n):
+  """Helper for computing debiased dot product similarity (i.e. linear HSIC)."""
+  # This formula can be derived by manipulating the unbiased estimator from
+  # Song et al. (2007).
+  return (
+      xty - n / (n - 2.) * sum_squared_rows_x.dot(sum_squared_rows_y)
+      + squared_norm_x * squared_norm_y / ((n - 1) * (n - 2)))
+
+
+def feature_space_linear_cka(features_x, features_y, debiased=False):
+  """Compute CKA with a linear kernel, in feature space.
+
+  This is typically faster than computing the Gram matrix when there are fewer
+  features than examples.
+
+  Args:
+    features_x: A num_examples x num_features matrix of features.
+    features_y: A num_examples x num_features matrix of features.
+    debiased: Use unbiased estimator of dot product similarity. CKA may still be
+      biased. Note that this estimator may be negative.
+
+  Returns:
+    The value of CKA between X and Y.
+  """
+  features_x = features_x - np.mean(features_x, 0, keepdims=True)
+  features_y = features_y - np.mean(features_y, 0, keepdims=True)
+
+  dot_product_similarity = np.linalg.norm(features_x.T.dot(features_y)) ** 2
+  normalization_x = np.linalg.norm(features_x.T.dot(features_x))
+  normalization_y = np.linalg.norm(features_y.T.dot(features_y))
+
+  if debiased:
+    n = features_x.shape[0]
+    # Equivalent to np.sum(features_x ** 2, 1) but avoids an intermediate array.
+    sum_squared_rows_x = np.einsum('ij,ij->i', features_x, features_x)
+    sum_squared_rows_y = np.einsum('ij,ij->i', features_y, features_y)
+    squared_norm_x = np.sum(sum_squared_rows_x)
+    squared_norm_y = np.sum(sum_squared_rows_y)
+
+    dot_product_similarity = _debiased_dot_product_similarity_helper(
+        dot_product_similarity, sum_squared_rows_x, sum_squared_rows_y,
+        squared_norm_x, squared_norm_y, n)
+    normalization_x = np.sqrt(_debiased_dot_product_similarity_helper(
+        normalization_x ** 2, sum_squared_rows_x, sum_squared_rows_x,
+        squared_norm_x, squared_norm_x, n))
+    normalization_y = np.sqrt(_debiased_dot_product_similarity_helper(
+        normalization_y ** 2, sum_squared_rows_y, sum_squared_rows_y,
+        squared_norm_y, squared_norm_y, n))
+
+  return dot_product_similarity / (normalization_x * normalization_y)
+
+
+
+if __name__ == "__main__":
+    a_checkpoint_3 = np.load("/srv/share/svanga3/bert-representations/sst_2/extracted/checkpoint-3500.npy")
+    b_checkpoint_3 = np.load("/srv/share/svanga3/bert-representations/sst_different_seed/extracted/checkpoint-3500.npy")
+    X = a_checkpoint_3.astype(np.float64)
+    Y = b_checkpoint_3.astype(np.float64)
+    import seaborn as sns;
+    import matplotlib.pyplot as plt
+    sim = np.zeros((12, 12))
+    for dim_x in range(12):
+        for dim_y in range(12):
+            X_1 = X[:, dim_x, :].squeeze()
+            Y_1 = Y[:, dim_y, :].squeeze()
+            sim[dim_x, dim_y] = feature_space_linear_cka(X_1, Y_1)
+    ax = sns.heatmap(sim)
+    plt.title("heatmap title")
+    ax.invert_yaxis()
